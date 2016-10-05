66bb5b2d1930ea52399715fccfed23485ad481da
Seth Bergman <seth.atxwebs@gmail.com>
2016-10-05 05:14:22 -0500

    Added mail service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { MailService } from './mail.service';
 
 @Component({
   selector: 'app-root',
   template: `<div>
 <app-simple-form></app-simple-form>
+
+{{mail.message}}
+
   </div>`
 })
+
 export class AppComponent {
   title = 'Angular 2 Fun!';
+
+  constructor(private mail:MailService){
+
+  }
 }
